13f2b575da5b2e65b19278357f959e113a0de489
Avinash Jha <info@easebuzz.in>
2023-08-09 11:22:33 +0530

    changed function call

--- Classes/TestPodsNew.swift
+++ Classes/TestPodsNew.swift
@@ -6,29 +6,16 @@
 //
 
 import Foundation
-import UIKit
 
-enum floatingLabelMovement: CGFloat {
-    case normal = 14
-    case small = 10
-}
-
-enum actionType {
-    case onTextFocusStart
-    case onTextFocusEnd
-    case onTextChange
-}
-
-@IBDesignable public class EditTextField: UITextField {
-    public func PrintTexts(name: String, age: Int, salary: Float) {
-        print("age is \(age)")
-        print("name is \(name)")
-        print("salary is \(salary)")
+public class YourLibrary {
+    
+    public init() {
+        // Initialization code
     }
     
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    public func printUserData(name: String, age: Int) {
+        print("User's name: \(name)")
+        print("User's age: \(age)")
     }
 }
-
 
